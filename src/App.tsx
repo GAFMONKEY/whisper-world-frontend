065d0b9d726e21e8baeea682f0bf3dcd79e2dfc2
Dennis Ginter <gide1011@h-ka.de>
2025-06-05 16:19:49 +0200

    installed tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Home />} />
       <Route path="/about" />
     </Routes>
   )
